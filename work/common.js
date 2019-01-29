4d58a6321f8843923f6ad2d3d3af23323eb10e67
Romasev M <mikhail.romasev@yandex.ru>
2019-01-29 23:22:53 +0400

    homework, 29/01/2019 corrected

--- work/common.js
+++ work/common.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -12,15 +12,68 @@ let appData = {
     savings: false
 };
 
-let question1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    question2 = prompt("Во сколько обойдется?", ''),
-    question3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    question4 = prompt("Во сколько обойдется?", '');
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
 
-appData.expenses.question1 = question2;
-appData.expenses.question3 = question4;
+    if ((typeof(a)) === "string" && (typeof(a) != null ) && (typeof(b) != null ) && a != "" && b != "" && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        console.log("Mistaked");
+        alert("Заполните данные ещё раз! Будьте внимательны!")
+        i--;
+    }
+};
 
-let myResult = alert(appData.allCash / 30);
 
-// console.log(appData);
-// console.log(myResult);
+
+// USE THE WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+//     i++;
+
+//     if ((typeof(a)) === "string" && (typeof(a) != null ) && (typeof(b) != null ) && a != "" && b != "" && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+//     }
+// }
+
+
+
+// USE THE DO
+
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько обойдется?", '');
+//     i++;
+
+//     if ((typeof(a)) === "string" && (typeof(a) != null ) && (typeof(b) != null ) && a != "" && b != "" && a.length < 50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else {
+//     }
+// } while (i < 2);
+
+
+
+appData.moneyOneDay = appData.allCash / 30;
+alert("Eжедневный бюджет " + appData.moneyOneDay);
+
+if (appData.moneyOneDay < (9691 / 30)) {
+    console.log("Ниже прожиточного минимума на душу населения");
+} else if ((appData.moneyOneDay > (9691 / 30)) && (appData.moneyOneDay < (9691 / 10))) {
+    console.log("Выше прожиточного минимума на душу населения");
+} else if (appData.moneyOneDay > (9691 / 10)) {
+    console.log("Высокий уровень достатка, минимум в 3 раза выше прожиточного минимума");
+} else {
+    console.log("Произошла ошибка")
+};
+
+console.log(appData.moneyOneDay);
+console.log(appData);
